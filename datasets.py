5e381271f640fa16ae20f874247c166e21ef74e3
JoeLi12345 <joe5050.li@gmail.com>
2022-12-12 22:09:40 -0800

    masking, better partitioning, datasets class for finetuning

--- datasets.py
+++ datasets.py
@@ -1,13 +1,14 @@
 import numpy as np
 import os, glob, cv2, sys
 import torch
+import random
 from pandas import read_csv
 
 class HAR_dataset(torch.utils.data.Dataset):
-	def __init__(self, std=0, mean=0):
-		self.std = std
-		self.mean = mean
+	def __init__(self, transform=None):
+		self.transform = transform
 		self.load_dataset()
+		self.format_data()
 
 	def load_dataset(self):
 		subjects = list()
@@ -20,16 +21,20 @@ class HAR_dataset(torch.utils.data.Dataset):
 			values = df.values[:,1:]
 			subjects.append(values)
 		self.subject = subjects[0]
-		self.format_data()
 	#sliding window data preparation
 	
 	def format_data(self):
+		print(len(self.subject))
 		dx = 64
 		cur = 0
 		inc = 1
+		num_activities = 8
 		self.data = []
+		category = []
+		for i in range(num_activities):
+			category.append([])
 		while (cur+dx-1 < len(self.subject)):
-			value = self.subject[cur][3]
+			value = round(self.subject[cur][3])
 			add = True
 			x_vals = []
 			y_vals = []
@@ -38,99 +43,138 @@ class HAR_dataset(torch.utils.data.Dataset):
 				x_vals.append(self.subject[i][0])
 				y_vals.append(self.subject[i][1])
 				z_vals.append(self.subject[i][2])
-				'''if (self.subject[i][3] != value):
+				if (self.subject[i][3] != value):
 					add = False
-					break'''
+					break
 			if (add):
-				self.data.append([x_vals, y_vals, z_vals])
+				category[value].append([x_vals, y_vals, z_vals])
 			cur += inc
+		train_data, val_data, test_data = [], [], []
+		for i in range(num_activities):
+			if len(category[i]) == 0:
+				continue
+			#print(i, ":", len(category[i]))
+			train_cnt = int(0.7*len(category[i]))
+			val_cnt = int(0.15*len(category[i]))
+			test_cnt = len(category[i])-train_cnt-val_cnt
+			for j in range(train_cnt):
+				train_data.append(category[i][j])
+			for j in range(train_cnt, train_cnt+val_cnt):
+				val_data.append(category[i][j])
+			for j in range(train_cnt+val_cnt, train_cnt+val_cnt+test_cnt):
+				test_data.append(category[i][j])
+		self.train_sz = len(train_data)
+		self.val_sz = len(val_data)
+		self.test_sz = len(test_data)
+		for i in train_data:
+			self.data.append(i)
+		for i in val_data:
+			self.data.append(i)
+		for i in test_data:
+			self.data.append(i)
+			
 	
 	def __getitem__(self, index):
 		reg = self.data[index]
-		#reg = torch.tensor(reg, dtype=torch.float).unsqueeze(0)
+		noised = reg
+		subset_indices = random.sample(range(len(reg)), int(random.uniform(0, 0.15)*len(reg)))
+		for i in subset_indices:
+			noised[i] = [-1, -1, -1]
 		reg = torch.tensor(reg, dtype=torch.float)
-		#add something with noise
-		noised = reg + torch.randn(reg.size(), dtype=torch.float) * self.std + self.mean
+		noised = torch.tensor(noised, dtype=torch.float)
+		if self.transform:
+			reg = self.transform(reg)
+			noised = self.transform(noised)
 		return reg, noised
 
 	def __len__(self):
 		return len(self.data)
-#test = HAR_dataset()
-#print(len(test.subject))
 
-'''
-class DAE_dataset(Dataset):
-	def __init__(self, data_dir, transform = None):
-		self.data_dir = data_dir
-		self.transform = transform
-		self.imgs_data	   = self.get_data(os.path.join(self.data_dir, 'imgs'))
-		self.noisy_imgs_data = self.get_data(os.path.join(self.data_dir, 'noisy'))
-		
-	def get_data(self, data_path):
-		data = []
-		for img_path in glob.glob(data_path + os.sep + '*'):
-			data.append(img_path)
-		return data
-	
-	def __getitem__(self, index):  
-		# read images in grayscale, then invert them
-		img	   = cv2.imread(self.imgs_data[index] ,0)
-		noisy_img = cv2.imread(self.noisy_imgs_data[index] ,0)
-	
-		if self.transform is not None:			
-			img = self.transform(img)			 
-			noisy_img = self.transform(noisy_img)  
 
-		return img, noisy_img
+class HAR_dataset_fine(HAR_dataset):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.load_dataset()
+		self.format_data()
+
+	def load_dataset(self):
+		subjects = list()
+		directory = '../HAR/'
+		for name in os.listdir(directory):
+			filename = directory + '/' + name
+			if not filename.endswith('.csv'):
+				continue
+			df = read_csv(filename, header = None)
+			values = df.values[:,1:]
+			subjects.append(values)
+		self.subject = subjects[0]
+	#sliding window data preparation
+	
+	def format_data(self):
+		print(len(self.subject))
+		dx = 64
+		cur = 0
+		inc = 1
+		num_activities = 8
+		self.data = []
+		self.labels = []
+		category = []
+		for i in range(num_activities):
+			category.append([])
+		while (cur+dx-1 < len(self.subject)):
+			value = round(self.subject[cur][3])
+			add = True
+			x_vals = []
+			y_vals = []
+			z_vals = []
+			for i in range(cur, cur+dx):
+				x_vals.append(self.subject[i][0])
+				y_vals.append(self.subject[i][1])
+				z_vals.append(self.subject[i][2])
+				if (self.subject[i][3] != value):
+					add = False
+					break
+			if (add):
+				category[value].append(([x_vals, y_vals, z_vals], value))
+			cur += inc
+		train_data, val_data, test_data = [], [], []
+		for i in range(num_activities):
+			if len(category[i]) == 0:
+				continue
+			#print(i, ":", len(category[i]))
+			train_cnt = int(0.7*len(category[i]))
+			val_cnt = int(0.15*len(category[i]))
+			test_cnt = len(category[i])-train_cnt-val_cnt
+			for j in range(train_cnt):
+				train_data.append(category[i][j])
+			for j in range(train_cnt, train_cnt+val_cnt):
+				val_data.append(category[i][j])
+			for j in range(train_cnt+val_cnt, train_cnt+val_cnt+test_cnt):
+				test_data.append(category[i][j])
+		self.train_sz = len(train_data)
+		self.val_sz = len(val_data)
+		self.test_sz = len(test_data)
+		for i in train_data:
+			self.data.append(i[0])
+			self.labels.append(i[1])
+		for i in val_data:
+			self.data.append(i[0])
+			self.labels.append(i[1])
+		for i in test_data:
+			self.data.append(i[0])
+			self.labels.append(i[1])
+			
+	def __getitem__(self, index):
+		reg = self.data[index]
+		reg = torch.tensor(reg, dtype=torch.float)
+		activity = self.labels[index]
+		#print("act:\n", activity)
+		activity = torch.tensor(activity, dtype=torch.int64)
+		#print("reg:\n", reg)
+		#print("act:\n", activity)
+		if self.transform:
+			reg = self.transform(reg)
+		return reg, activity
 
 	def __len__(self):
-		return len(self.imgs_data)
-	
-class custom_test_dataset(Dataset):
-	def __init__(self, data_dir, transform = None, out_size = (64, 256)):
-		assert out_size[0] <= out_size[1], 'height/width of the output image shouldn\'t not be greater than 1'
-		self.data_dir = data_dir
-		self.transform = transform
-		self.out_size = out_size
-		self.imgs_data	   = self.get_data(self.data_dir)
-
-	def get_data(self, data_path):
-		data = []
-		for img_path in glob.glob(data_path + os.sep + '*'):
-			data.append(img_path)
-		return data
-	
-	def __getitem__(self, index):  
-		# read images in grayscale, then invert them
-		img	   = cv2.imread(self.imgs_data[index] ,0)
-				
-		# check if img height exceeds out_size height
-		if img.shape[0] > self.out_size[0]:
-			resize_factor = self.out_size[0]/img.shape[0]
-			img = cv2.resize(img, (0, 0), fx=resize_factor, fy=resize_factor)
-
-		# check if img width exceeds out_size width
-		if img.shape[1] > self.out_size[1]:
-			resize_factor = self.out_size[1]/img.shape[1]
-			img = cv2.resize(img, (0, 0), fx=resize_factor, fy=resize_factor)
-	   
-		# add padding where required
-		# pad height
-		pad_height = self.out_size[0] - img.shape[0]
-		pad_top = int(pad_height/2)
-		pad_bottom = self.out_size[0] - img.shape[0] - pad_top
-		# pad width
-		pad_width = self.out_size[1] - img.shape[1]
-		pad_left = int(pad_width/2)
-		pad_right = self.out_size[1] - img.shape[1] - pad_left
-		
-		img = np.pad(img, ((pad_top, pad_bottom), (pad_left, pad_right)), constant_values=(0,0))	
-		
-		if self.transform is not None:			
-			img = self.transform(img)	  
-		
-		return img
-
-	def __len__(self):
-		return len(self.imgs_data)
-'''
+		return len(self.data)
